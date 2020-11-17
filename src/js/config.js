50645b5ee50b7a11dfc4382c79d8b5f0e9360fe1
Maribel Morales <mmc@logify.com.mx>
2020-11-17 09:28:48 -0600

    actalizar version 2.3.13.3

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 var config = {
      //URL_WS: "http://localhost:8888/desarrollo.api.logify.com.mx/",
-    URL_WS: "https://desarrollo.api.logify.com.mx/",
-    //URL_WS: "https://api.logify.com.mx/",
+    //URL_WS: "https://desarrollo.api.logify.com.mx/",
+    URL_WS: "https://api.logify.com.mx/",
     //URL_NEW_WS:"https://logisticus.logify.com.mx/",
     QUALITY: 60
 };
